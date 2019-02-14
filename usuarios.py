2d1617c538d0c5a40edb58d635a94e68307772fa
Flavio <flaviogpacheco@hotmail.com>
2019-02-13 22:24:56 -0200

    exercicio final da aula 3

---
+++ usuarios.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#Abrir o arquivos usuários
+# Seperar os valores por ","
+# E escrever na tela o dicionario:
+# {"nome : "Hector", "idade" : 27, "email" : hecto.silva@4linux.com.br}
+
+#arquivo = open('arquivos.csv')
+#for linha in arquivo:
+#print(linha{"nome" : 
+
+for l in open('usuarios.csv'):
+    # aqui eu preciso separar por ","
+    nome, idade, email = l.split(',')
+    print({
+        "nome"  : nome.strip(), 
+        "email" : email.strip(),
+        "idade" : int(idade.strip())
+        })    
